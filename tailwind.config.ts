4f7c2c01ac97cf846cd5b59b50da6012400b1a71
Krisna Rusdiono <krisnarusdiono@gmail.com>
2025-05-07 16:32:49 +0700

    feat: setup mui library and refactor structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // important: '#__next',
+  corePlugins: {
+    preflight: false,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
