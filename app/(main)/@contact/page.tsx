fb802bf9ab57790506d1b090ef5217d6f48cb8c2
Krisna Rusdiono <38303737+krisnaRusdiono@users.noreply.github.com>
2025-05-10 16:19:08 +0700

    feat: add metadata, resolve trimmed image, update wording text and update contacts (#9)
    * fix: resolve trimmed project img
    
    * fix: add animation on some contents
    
    * fix: switch web developer to software engineer
    
    * feat: update metadata
    
    * feat: add email on contacts

--- app/(main)/@contact/page.tsx
+++ app/(main)/@contact/page.tsx
@@ -1,4 +1,5 @@
 import ContentContainer from '@/components/ContentContainer';
+import TransitionEnter from '@/components/Transition/Enter';
 import CONTACT_DATA from '@/constants/contactData';
 import { Divider, Typography } from '@mui/material';
 import Image from 'next/image';
@@ -11,41 +12,46 @@ const ContactSlot = () => (
     className='flex-col [&>p]:text-center gap-8 mx-auto max-w-[60rem]'
   >
     <div className='mt-24'>
-      <Typography className='text-center'>
-        Glad you read this 🙂, feel free to reach me on these platform!
-      </Typography>
+      <TransitionEnter delay={0.1}>
+        <Typography className='text-center'>
+          Glad you read this 🙂, feel free to reach me on these platform!
+        </Typography>
+      </TransitionEnter>
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 py-8'>
-        {CONTACT_DATA.map(({ name, icon, url, value }) => (
-          <Link
-            href={url}
-            key={name}
-            className='flex items-center gap-3 text-inherit no-underline hover:no-underline focus:outline-none p-4 bg-slate-200 rounded-full shadow-lg hover:shadow-md transition-all hover:bg-slate-50'
-            target='_blank'
-          >
-            <Image
-              src={icon}
-              alt={`${name} icon`}
-              width={32}
-              height={32}
-              className='[&>*]:fill-black'
-            />
-            <Divider orientation='vertical' />
-            <Typography className='overflow-auto simple-scrollbar'>
-              {value}
-            </Typography>
-          </Link>
+        {CONTACT_DATA.map(({ name, icon, url, value }, i) => (
+          <TransitionEnter key={name} delay={i / 3}>
+            <Link
+              href={url}
+              className='flex items-center gap-3 text-inherit no-underline hover:no-underline focus:outline-none p-4 bg-slate-200 rounded-full shadow-lg hover:shadow-md transition-all hover:bg-slate-50'
+              target='_blank'
+            >
+              <Image
+                src={icon}
+                alt={`${name} icon`}
+                width={32}
+                height={32}
+                className='[&>*]:fill-black'
+              />
+              <Divider orientation='vertical' />
+              <Typography className='overflow-auto simple-scrollbar'>
+                {value}
+              </Typography>
+            </Link>
+          </TransitionEnter>
         ))}
       </div>
-      <Typography className='text-center'>
-        And for HR, i am more than happy to share with you my latest CV that you
-        can download
-        <a
-          className='min-w-0 px-1 lowercase text-inherit hover:!bg-transparent'
-          href='/api/download'
-        >
-          here
-        </a>
-      </Typography>
+      <TransitionEnter delay={0.6}>
+        <Typography className='text-center'>
+          And for HR, i am more than happy to share with you my latest CV that
+          you can download
+          <a
+            className='min-w-0 px-1 lowercase text-inherit hover:!bg-transparent'
+            href='/api/download'
+          >
+            here
+          </a>
+        </Typography>
+      </TransitionEnter>
     </div>
   </ContentContainer>
 );
